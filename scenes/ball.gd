771d5e6ef8cc9750d0556d2e04fb5be18aa3a1d4
Hanya You <youh@catlin.edu>
2025-11-06 16:16:06 -0800

    resets ball when it falls into the void

--- scenes/ball.gd
+++ scenes/ball.gd
@@ -2,15 +2,19 @@ extends PhysicsBody2D
 var collisionInfo = KinematicCollision2D
 var veloBall = Vector2()
 var speed = 1
-@onready var walls = $"../walls/CollisionPolygon2D"
+@onready var walls = $"../walls"
+@onready var floor = $"../floor"
 
 
 func _physics_process(delta: float) -> void:
 	veloBall.y += 1
 	collisionInfo = move_and_collide(veloBall)
 	if collisionInfo:
-		if collisionInfo.get_collider_shape() == walls:
+		if collisionInfo.get_collider() == walls:
 			veloBall.x *= -0.7
+		else: if collisionInfo.get_collider() == floor:
+			global_position=Vector2(0,-300)
+			veloBall = Vector2(0, 0)
 		else:
 			veloBall.x += collisionInfo.get_collider_velocity().x/50
 			veloBall.y = -40
